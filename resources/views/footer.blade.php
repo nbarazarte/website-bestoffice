03b0e419c3266f4116c2ac1f0c937cfe267d2219
jaennie <jaennie@debian-jaennie.cantv.net>
2018-02-20 16:55:19 -0400

    nuevo logo

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -10,7 +10,7 @@
 								<li>
 
 									<a href="http://www.solucionesbestoffice.com" target="">
-										<img class="img-responsive" src="{{ asset('smarty/HTML/assets/images/logotipoBO2.png') }}" alt="logo BO" title="www.solucionesbestoffice.com" style="width: 100%" />	
+										<img class="img-responsive" src="{{ asset('smarty/HTML/assets/images/BO_NuevoLogotipo.png') }}" alt="logo BO" title="www.solucionesbestoffice.com" style="width: 50%" />	
 									</a>
 
 								</li>
